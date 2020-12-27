b6936ee072ca5d43ca49c55435a3f849fb7f48ce
xiaflores <38366041+xiaflores@users.noreply.github.com>
2020-12-26 22:58:07 -0400

    mi primer commit

---
+++ app.js
@@ -0,0 +1,17 @@
+//console.log(console.log('hola mundo'));
+let a = 10,
+    b = 20,
+    c = 'hola ',
+    d = 'como estas',
+    x = a + c;
+
+const saludo=c+d;
+
+console.log('%c Mis variables','color:blue; font-weight:bold');
+console.log({a});
+console.log({b});
+console.log({c});
+console.log({d});
+//c='hola de nuevo ';
+
+console.table({a,b,c,d});
